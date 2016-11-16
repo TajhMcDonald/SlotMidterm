169d1eacaf5df61a9fee7d5777886427a49cebbe
Tajh McDonald <tajhmcdonald@tajhs-air.lehman.edu>
2016-11-16 18:46:23 -0500

    Initial Commit

---
+++ SlotMidterm/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SlotMidterm
+//
+//  Created by Tajh McDonald on 11/16/16.
+//  Copyright © 2016 McDonald's Computing INC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SlotMidterm/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SlotMidterm
+//
+//  Created by Tajh McDonald on 11/16/16.
+//  Copyright © 2016 McDonald's Computing INC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
